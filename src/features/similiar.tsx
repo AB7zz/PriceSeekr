a8711a91b445bb1fb83abade6da85812f843b18d
AB7zz <abhinavcv007@gmail.com>
2023-07-06 22:38:42 +0530

    added style and displays all similiar products

--- src/features/similiar.tsx
+++ src/features/similiar.tsx
@@ -11,7 +11,7 @@ const Similiar = ({data}) => {
         api_key: "9ff5a1b75caee5bb01410bebc61e1014f53a01e8dfa29b28d2e7f23067c0338f", 
         q: title,       
         engine: 'google_shopping',             
-        location: "Austin, TX",            
+        location: "Kochi, Kerala, India",            
       }
       let result = search.json(params, (data) => {
         console.log(data["shopping_results"])
@@ -23,14 +23,31 @@ const Similiar = ({data}) => {
     }
   }, [data])
   return (
-    <div>
-      {similiar && 
-        similiar.map((product, i) => (
-          <div key={i}>
-            <h1 className="text-xl font-bold">{product.title}</h1>
+    <div className="px-5 py-5 w-[500px] bg-slate-100">
+      <div>
+        <h1 className='text-xl font-bold my-3'>Your product</h1>
+        {data && 
+          <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
+            <img className='w-[200px]' src={data[2]} alt="" />
+            <h3 className='text-lg'>{data[0]}</h3>
+            <p className='text-blue-500 text-lg font-bold'>{data[1]}</p>
           </div>
-        ))
-      }
+        }
+      </div>
+      <div>
+        <h1 className='text-xl font-bold my-3'>Similiar products</h1>
+        <div className='grid grid-cols-2 gap-4'>
+          {similiar && 
+            similiar.map((product, i) => (
+              <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
+                <img className='w-[200px]' src={product.thumbnail} alt="product thumbnail" />
+                <a href={product.link} className="text-lg">{product.title}</a>
+                <p className="text-blue-500 text-lg font-bold">{product.price}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
--- src/features/table.tsx
+++ src/features/table.tsx
@@ -9,34 +9,34 @@ const Table = ({data}) => {
     }
   }, [data])
   return (
-    <>
-    <h1 className='text-2xl text-center py-5'>Product Details</h1>
-    <div className="flex items-center justify-center px-5 pb-5">
-      <table className="table-auto">
-        <thead>
-          <tr className="border">
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th className='pr-[320px]'>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data ? <tr className="border">
-            <td><img className="!max-w-[100px]" src={data[2]} /></td>
-            <td>{data[0]}</td>
-            <td>{data[1]}</td>
-            <td id="desc"></td>
-          </tr> : <tr className="border">
-            <td><img src="" /></td>
-            <td>Random Title testing</td>
-            <td>Random price</td>
-            <td>Random Description</td>
-          </tr>}
-        </tbody>
-      </table>
+    <div className='bg-slate-100'>
+      <h1 className='text-2xl text-center py-5'>Product Details</h1>
+      <div className="flex items-center justify-center px-5 pb-5">
+        <table className="table-auto bg-white rounded-[15px]">
+          <thead>
+            <tr className="border">
+              <th className='py-3'>Image</th>
+              <th className='py-3'>Title</th>
+              <th className='py-3'>Price</th>
+              <th className=' py-3 pr-[320px]'>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data ? <tr className="border">
+              <td><img className="!max-w-[100px]" src={data[2]} /></td>
+              <td>{data[0]}</td>
+              <td>{data[1]}</td>
+              <td id="desc"></td>
+            </tr> : <tr className="border">
+              <td><img src="" /></td>
+              <td>Random Title testing</td>
+              <td>Random price</td>
+              <td>Random Description</td>
+            </tr>}
+          </tbody>
+        </table>
+      </div>
     </div>
-    </>
   )
 }
 
