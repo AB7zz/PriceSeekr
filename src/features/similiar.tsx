e97ee2fc7da9c1dd8ff16501928641ec5ea15bee
AB7zz <abhinavcv007@gmail.com>
2023-07-10 20:31:13 +0530

    location is dynamic

--- src/features/similiar.tsx
+++ src/features/similiar.tsx
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import SerpApi from "google-search-results-nodejs"
 const search = new SerpApi.GoogleSearch("9ff5a1b75caee5bb01410bebc61e1014f53a01e8dfa29b28d2e7f23067c0338f")
-
+import axios from 'axios'
+import google_domains from '../json/google-domains.json'
 
 const Similiar = ({data}) => {
   const [similiar, setSimiliar] = useState(null)
   useEffect(() => {
-    const searchTitle = async(title) => {
+    const searchTitle = async(title, country) => {
       let params = {
         api_key: "9ff5a1b75caee5bb01410bebc61e1014f53a01e8dfa29b28d2e7f23067c0338f", 
         q: title,       
+        gl: country,
+        google_domain: google_domains.find(g => g.country_code == country)?.domain,
         engine: 'google_shopping',             
-        location: "Kochi, Kerala, India",            
       }
       let result = search.json(params, (data) => {
         console.log(data["shopping_results"])
         setSimiliar(data["shopping_results"])
       })
     }
+
+    const getLocation = async() => {
+      try{
+        const response = await axios.get("https://ipinfo.io");
+        return response.data.country.toLowerCase()
+      }catch(error){
+        console.log(error)
+      }
+    }
     if(data){
-      searchTitle(data[0])
+      getLocation()
+        .then(country => searchTitle(data[0], country))
     }
   }, [data])
   return (
@@ -39,7 +51,7 @@ const Similiar = ({data}) => {
         <div className='grid grid-cols-2 gap-4'>
           {similiar && 
             similiar.map((product, i) => (
-              <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
+              <div key={i} className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
                 <img className='w-[200px]' src={product.thumbnail} alt="product thumbnail" />
                 <a href={product.link} className="text-lg">{product.title}</a>
                 <p className="text-blue-500 text-lg font-bold">{product.price}</p>
