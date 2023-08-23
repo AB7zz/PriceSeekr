2f25917caaa5a734e14f5b11dcd7ebe427a76898
AB7zz <48093418+AB7zz@users.noreply.github.com>
2023-08-23 14:43:52 -0400

    Updated package.json, package-lock.json, and same.tsx

--- src/features/same.tsx
+++ src/features/same.tsx
@@ -1,29 +1,65 @@
-import React, { useEffect, useState } from 'react'
-const SerpApi = require("google-search-results-nodejs")
-const search = new SerpApi.GoogleSearch("4ede514098f0aaed97b7659099bcebc41d4015a987a506f23ab7a6c4be65063f")
+import React, { useEffect, useState } from 'react';
+import axios, * as others from 'axios';
+const SerpApi = require("google-search-results-nodejs");
 
+const search = new SerpApi.GoogleSearch("4ede514098f0aaed97b7659099bcebc41d4015a987a506f23ab7a6c4be65063f");
 
-const Same = ({data}) => {
-  const [same, setSame] = useState(null)
+const Same = ({ data }) => {
+  const [same, setSame] = useState(null);
+
   useEffect(() => {
-    const searchImage = async(image) => {
-      console.log(image)
+    const searchImage = async (image) => {
+      console.log(image);
       let params = {
-        api_key: "4ede514098f0aaed97b7659099bcebc41d4015a987a506f23ab7a6c4be65063f", 
-        url: image,       
-        engine: 'google_lens',     
-        location: "Kochi, Kerala, India",            
-      }
-      let result = search.json(params, (data) => {
-        console.log(data["visual_matches"])
-        setSame(data["visual_matches"])
-        console.log(same)
-      })
+        api_key: "4ede514098f0aaed97b7659099bcebc41d4015a987a506f23ab7a6c4be65063f",
+        url: image,
+        engine: 'google_lens',
+        location: "Kochi, Kerala, India",
+      };
+
+      let result = search.json(params, async (data) => {
+        if (data && data["visual_matches"] && data["visual_matches"].length > 0) {
+          // Filter out items that don't have a "price" property
+          const itemsWithPrice = data["visual_matches"].filter(item => item.price);
+
+          // Use Promise.all to fetch webpage content for each item
+          const itemPromises = itemsWithPrice.map(async (item) => {
+            try {
+              const response = await axios.get(item.link);
+              item.pageContent = response.data; // Store the webpage content
+              return item;
+            } catch (error) {
+              console.error(`Error fetching ${item.link}: ${error.message}`);
+              return null; // Handle errors gracefully
+            }
+          });
+
+          const itemsWithPageContent = await Promise.all(itemPromises);
+
+          // Filter out items that contain "out of stock" in their webpage content
+          const filteredItems = itemsWithPageContent.filter(item => {
+            if (item.pageContent) {
+              const pageContentLower = item.pageContent.toLowerCase();
+              return !(
+                pageContentLower.includes('out of stock') ||
+                pageContentLower.includes('no longer available') || 
+                pageContentLower.includes('sold out')
+              );
+            }
+            return true; // Include items without webpage content
+          });
+          // Sort the filtered items by price in ascending order
+          const sortedItems = filteredItems.sort((a, b) => a.price.extracted_value - b.price.extracted_value);
+          setSame(sortedItems);
+        }
+      });
+    };
+
+    if (data) {
+      searchImage(data[2]);
     }
-    if(data){
-      searchImage(data[2])
-    }
-  }, [data])
+  }, [data]);
+
   return (
     <div className="px-5 py-5 w-[500px] bg-slate-100">
       <div>
