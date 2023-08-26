6df9e8e6547092f4ffdb078622ae6e8c48e54ec1
AB7zz <abhinavcv007@gmail.com>
2023-08-27 00:28:58 +0530

    structured

--- src/firebase/hooks.tsx
+++ src/firebase/hooks.tsx
@@ -1,43 +1,58 @@
 import {
-    getAuth,
     onAuthStateChanged,
     GoogleAuthProvider,
     signInWithPopup,
     signOut,
     signInWithCredential,
 } from "firebase/auth";
-import { app, auth } from "~firebase"
+import { useSearchContext } from "~context/SearchContext";
+import { auth } from "~firebase"
 
 export const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      console.error("Sign out error:", error);
-    }
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Sign out error:", error);
+  }
+};
+
+export const useDetectChange = () => {
+  const { setUser } = useSearchContext();
+
+  const handleDetectChange = () => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log(user);
+    });
+
+    return () => unsubscribe();
+  };
+
+  return handleDetectChange;
 };
 
 export const handleGoogleLogin = async () => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("User is already logged in:", user);
-      } else {
-        console.log("No user is currently logged in.");
-        chrome.identity.getAuthToken({ interactive: true }, async function (token) {
-          if (chrome.runtime.lastError || !token) {
-            console.error(chrome.runtime.lastError)
-            return
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("User is already logged in:", user);
+    } else {
+      console.log("No user is currently logged in.");
+      chrome.identity.getAuthToken({ interactive: true }, async function (token) {
+        if (chrome.runtime.lastError || !token) {
+          console.error(chrome.runtime.lastError)
+          return
+        }
+        if (token) {
+          // const credential = new GoogleAuthProvider()
+          const credential = GoogleAuthProvider.credential(null, token)
+          try {
+            // await signInWithPopup(auth, credential)
+            await signInWithCredential(auth, credential)
+          } catch (e) {
+            console.error("Could not log in. ", e)
           }
-          if (token) {
-            // const credential = new GoogleAuthProvider()
-            const credential = GoogleAuthProvider.credential(null, token)
-            try {
-              // await signInWithPopup(auth, credential)
-              await signInWithCredential(auth, credential)
-            } catch (e) {
-              console.error("Could not log in. ", e)
-            }
-          }
-        })
-      }
-    });
-  };
+        }
+      })
+    }
+  });
+};
