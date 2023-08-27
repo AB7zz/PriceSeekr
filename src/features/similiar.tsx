4c6456643c9fe5d20793c61363004f47731a62a7
AB7zz <48093418+AB7zz@users.noreply.github.com>
2023-08-27 15:17:50 -0400

    similar loader

--- src/features/similiar.tsx
+++ src/features/similiar.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSearchContext } from '~context/SearchContext'
-
+import Loader from './loader';
 const Similiar = ({data}) => {
   const {runSearchSimiliar, similiar} = useSearchContext()
+  const [isLoading, setIsLoading] = useState(true);
+
   React.useEffect(() => {
+
     if(!similiar){
       runSearchSimiliar(data)
+    } else {
+      setIsLoading(false);
     }
-  }, [data])
+  }, [data, similiar]);
+
   return (
     <div className="px-5 py-5 w-[500px] bg-white">
-      <div>
+      {/* <div>
         <h1 className='text-xl font-bold my-3'>Your product</h1>
         {data && 
           <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
@@ -19,23 +25,25 @@ const Similiar = ({data}) => {
             <p className='text-blue-500 text-lg font-bold'>{data[2]}</p>
           </div>
         }
-      </div>
-      <div>
-        <h1 className='text-xl font-bold my-3'>Similiar products</h1>
-        <div className='grid grid-cols-2 gap-4'>
-          {similiar && 
-            similiar.map((product, i) => (
-              <div key={i} className='shadow-lg px-3 py-3 rounded-[15px] bg-gray-100'>
-                <img className='w-[200px]' src={product.thumbnail} alt="product thumbnail" />
-                <a href={product.link} className="text-lg">{product.title}</a>
-                <p className="text-blue-500 text-lg font-bold">{product.price}</p>
-              </div>
-            ))
-          }
+      </div> */}
+        <div>
+          {/* <h1 className='text-xl font-bold my-3'>Similiar products</h1> */}
+          <div className="grid grid-cols-2 gap-4">
+            {isLoading ? (
+              <Loader />
+              ) : (similiar && 
+              similiar.map((product, i) => (
+                <div key={i} className='shadow-lg px-3 py-3 rounded-[15px] bg-gray-100'>
+                  <img className='w-[200px]' src={product.thumbnail} alt="product thumbnail" />
+                  <a href={product.link} className="text-lg">{product.title}</a>
+                  <p className="text-blue-500 text-lg font-bold">{product.price}</p>
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default Similiar
