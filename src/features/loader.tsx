f5b9d3f50d71ae69e1a00799aa0b80cb600388f9
AB7zz <48093418+AB7zz@users.noreply.github.com>
2023-08-27 14:59:01 -0400

    loader

---
+++ src/features/loader.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const Loader = () => {
+  const [loadingText, setLoadingText] = useState('Searching for products');
+
+  useEffect(() => {
+    const textArray = [
+      'Searching for matches',
+      'Filtering the results',
+      'Compiling the results',
+    ];
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      currentIndex = (currentIndex + 1) % textArray.length;
+      if (currentIndex === textArray.length - 1) {
+        clearInterval(intervalId); // Stop interval at last index
+      }
+      setLoadingText(textArray[currentIndex]);
+    }, 2000); 
+
+
+  }, []);
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <div className="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-4 h-20 w-20 mx-auto mb-4"></div>
+      
+            <p className=" text-center text-gray-600 text-lg transition-opacity duration-500 ease-in-out">
+                {loadingText}
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/features/same.tsx
+++ src/features/same.tsx
@@ -1,44 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchContext } from '~context/SearchContext'
+import React, { useEffect, useState } from 'react';
+import { useSearchContext } from '~context/SearchContext';
+import Loader from './loader';
 
+const Same = ({ data }) => {
+  const { runSearchImage, same } = useSearchContext();
+  const [isLoading, setIsLoading] = useState(true);
 
-const Same = ({ data}) => {
-  const {runSearchImage, same} = useSearchContext()
   useEffect(() => {
-    if(!same){
-      runSearchImage(data)
+    if (!same) {
+      runSearchImage(data);
+    } else {
+      setIsLoading(false);
     }
-  }, [data]);
+  }, [data, same]);
+  
 
   return (
     <div className="px-5 py-5 w-[500px] bg-white">
-      <div>
-        <h1 className='text-xl font-bold my-3'>Your product</h1>
-        {data && 
-          <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
-            <img className='w-[200px]' src={data[1]} alt="" />
-            <h3 className='text-lg'>{data[0]}</h3>
-            <p className='text-blue-500 text-lg font-bold'>{data[2]}</p>
+      {/* <div>
+        <h1 className="text-xl font-bold my-3">Your product</h1>
+        {data && (
+          <div className="shadow-lg px-3 py-3 rounded-[15px] bg-white">
+            <img className="w-[200px]" src={data[1]} alt="" />
+            <h3 className="text-lg">{data[0]}</h3>
+            <p className="text-blue-500 text-lg font-bold">{data[2]}</p>
           </div>
-        }
-      </div>
+        )}
+      </div> */}
       <div>
-        <h1 className='text-xl font-bold my-3'>Same products</h1>
-        <div className='grid grid-cols-2 gap-4'>
-          {same && same.map((product, i) => (
-            <div className='shadow-lg px-3 py-3 rounded-[15px] bg-gray-100'>
-              <img className='w-[200px]' src={product.thumbnail} alt="product thumbnail" />
-              <a href={product.link} className="text-lg">{product.title}</a>
-              {product.price && (
-                <p className="text-blue-500 text-lg font-bold">{product.price.value}</p>
-              )}
-            </div>
-          ))}
+        {/* <h1 className="text-xl font-bold my-3">Same products</h1> */}
+        <div className="grid grid-cols-2 gap-4">
+          {isLoading ? (
+            <Loader />
+            ) : (
+            same &&
+            same.map((product, i) => (
+              <div className="shadow-lg px-3 py-3 rounded-[15px] bg-gray-100" key={i}>
+                <img className="w-[200px]" src={product.thumbnail} alt="product thumbnail" />
+                <a href={product.link} className="text-lg">
+                  {product.title}
+                </a>
+                {product.price && (
+                  <p className="text-blue-500 text-lg font-bold">{product.price.value}</p>
+                )}
+              </div>
+            ))
+          )}
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Same
+export default Same;
