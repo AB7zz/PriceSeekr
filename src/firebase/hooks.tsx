cd016f4fc7eb04f961b94d617a390a2f2a015027
AB7zz <abhinavcv007@gmail.com>
2023-08-30 18:58:41 +0530

    tried again

--- src/firebase/hooks.tsx
+++ src/firebase/hooks.tsx
@@ -39,45 +39,93 @@ export const useDetectChange = () => {
   return handleDetectChange;
 };
 
-
 export const useGoogleLogin = () => {
-  const {setUser, user} = useSearchContext()
-  const handleGoogleLogin = async() => {
+  const { setUser, user } = useSearchContext();
+
+  const handleGoogleLogin = async () => {
     if (user) {
       console.log("User is already logged in:", user);
-      setUser(user)
     } else {
       console.log("No user is currently logged in.");
-      chrome.identity.getAuthToken({ interactive: true }, async function (token) {
+      // const redirectURL = chrome.identity.getRedirectURL();
+      // const clientID =
+      //   "43535114296-5pjd4u7innrcvo39609hud4fupputtu1.apps.googleusercontent.com";
+      // const scopes = ["openid", "email", "profile"];
+      // let authURL = "https://accounts.google.com/o/oauth2/auth";
+      // authURL += `?client_id=${clientID}`;
+      // authURL += `&response_type=token`;
+      // authURL += `&redirect_uri=${encodeURIComponent(redirectURL)}`;
+      // authURL += `&scope=${encodeURIComponent(scopes.join(" "))}`;
+
+      // chrome.identity.launchWebAuthFlow({
+      //   interactive: true,
+      //   url: authURL,
+      // });
+          chrome.identity.getAuthToken({ interactive: true }, async function (token) {
         if (chrome.runtime.lastError || !token) {
-          console.error('Chrome error: ', chrome.runtime.lastError)
-          return
+          console.error(chrome.runtime.lastError);
+          return;
         }
         if (token) {
-          chrome.identity.removeCachedAuthToken({token:token},function(){
-            console.log('O',token)
-            chrome.identity.getAuthToken({ interactive: true }, async function (token2) {
-              if (chrome.runtime.lastError || !token2) {
-                console.error('Chrome error: ', chrome.runtime.lastError)
-                return
-              }
-              if (token2) {
-                console.log('N',token2)
-                const credential = GoogleAuthProvider.credential(null, token2)
-                try {
-                  const res = await signInWithCredential(auth, credential)
-                  setUser(res.user)
-                  console.log(res.user)
-                } catch (e) {
-                  console.error("Could not log in. ", e)
-                }
-              }
-            })
-          });
+          const credential = new GoogleAuthProvider();
+          // const credential = GoogleAuthProvider.credential(null, token)
+          try {
+            const res = await signInWithPopup(auth, credential);
+            setUser(res.user);
+            // await signInWithCredential(auth, credential)
+          } catch (e) {
+            console.error("Could not log in. ", e);
+          }
         }
-      })
+      });
     }
-  }
+  };
 
-  return handleGoogleLogin
+  return handleGoogleLogin;
 };
+
+
+
+
+// export const useGoogleLogin = () => {
+//   const {setUser, user} = useSearchContext()
+//   const handleGoogleLogin = async() => {
+//     if (user) {
+//       console.log("User is already logged in:", user);
+//       setUser(user)
+//     } else {
+//       console.log("No user is currently logged in.");
+//       chrome.identity.getAuthToken({ interactive: true }, async function (token) {
+//         if (chrome.runtime.lastError || !token) {
+//           console.error('Chrome error: ', chrome.runtime.lastError)
+//           return
+//         }
+//         if (token) {
+//           chrome.identity.removeCachedAuthToken({token:token},function(){
+//             console.log('O',token)
+//             chrome.identity.getAuthToken({ interactive: true }, async function (token2) {
+//               if (chrome.runtime.lastError || !token2) {
+//                 console.error('Chrome error: ', chrome.runtime.lastError)
+//                 return
+//               }
+//               if (token2) {
+//                 console.log('N',token2)
+                
+//                 const credential = GoogleAuthProvider.credential(null, token2)
+//                 try {
+//                   const res = await signInWithCredential(auth, credential)
+//                   setUser(res.user)
+//                   console.log(res.user)
+//                 } catch (e) {
+//                   console.error("Could not log in. ", e)
+//                 }
+//               }
+//             })
+//           });
+//         }
+//       })
+//     }
+//   }
+
+//   return handleGoogleLogin
+// };
