63792aa8e66f1ce12e5504ced2fa4dce0b0c6911
AB7zz <abhinavcv007@gmail.com>
2023-09-01 17:56:18 +0530

    good

--- src/components/BotNav.tsx
+++ src/components/BotNav.tsx
@@ -5,7 +5,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const BotNav = () => {
   return (
-    <div className='bg-[#FF9C1A] rounded-tr-[12px] rounded-tl-[15px] px-5 py-2 flex justify-between'>
+    <div className='sticky bottom-0 bg-[#FF9C1A] rounded-tr-[12px] rounded-tl-[15px] px-5 py-2 flex justify-between'>
         <HomeIcon className="text-white" />
         <HistoryIcon className="text-white" />
         <AccountCircleIcon className="text-white" />
--- src/features/similiar.tsx
+++ src/features/similiar.tsx
@@ -22,18 +22,7 @@ const Similiar = ({data}) => {
       </div> 
       : 
       <>
-        {/* <div>
-          <h1 className='text-xl font-bold my-3'>Your product</h1>
-          {data && 
-            <div className='shadow-lg px-3 py-3 rounded-[15px] bg-white'>
-              <img className='w-[200px]' src={data[1]} alt="" />
-              <h3 className='text-lg'>{data[0]}</h3>
-              <p className='text-blue-500 text-lg font-bold'>{data[2]}</p>
-            </div>
-          }
-        </div> */}
           <div>
-            {/* <h1 className='text-xl font-bold my-3'>Similiar products</h1> */}
             <div className="grid grid-cols-2 gap-4">
               {isLoading ? (
                 <Loader />
