275ea388d5aaaa51132ec2fffa043e8c1e075253
AB7zz <abhinavcv007@gmail.com>
2023-09-08 02:18:02 +0530

    close to fixing messaging bw background and content

--- src/backround/index.ts
+++ src/backround/index.ts
@@ -1,40 +1,5 @@
 export {}
-import type { PlasmoMessaging } from "@plasmohq/messaging"
-
-
-const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-    const message = "Hello!"
-   
-    res.send({
-      message
-    })
-}
-
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('its installed')
 })
-
-const filter = {
-    url: [
-      {
-        urlMatches: 'amazon',
-      }
-    ],
-};
-
-  
-chrome.webNavigation.onCompleted.addListener(async(details) => {
-    if(details.frameType == "outermost_frame"){
-        console.info("The user has loaded my favorite website!");
-        console.log(details)
-        const tabId = details.tabId
-        chrome.tabs.sendMessage(tabId, {message: "Message from background script"}, function (response) {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-            } else {
-                console.log('Message sent to content script:');
-            }
-        });
-    }
-}, filter);
--- src/contents/content.tsx
+++ src/contents/content.tsx
@@ -1,55 +1,39 @@
-import type { PlasmoCSConfig } from "plasmo"
-import styleText from "data-text:./style.css"
-import type { PlasmoGetStyle } from "plasmo"
-import logo from 'data-base64:~assets/icon.development.png'
-import { MySearchProvider, useSearchContext } from "~context/SearchContext"
-import React from 'react'
-
-// export const getStyle: PlasmoGetStyle = () => {
-//   const style = document.createElement("style")
-//   style.textContent = styleText
-//   return style
+// import { usePort } from "@plasmohq/messaging/hook"
+// import type { PlasmoCSConfig } from "plasmo"
+ 
+// export const config: PlasmoCSConfig = {
+//   matches: [
+//     "http://www.amazon.com/*",
+//     "https://www.amazon.com/*",
+//     "http://smile.amazon.com/*",
+//     "https://smile.amazon.com/*",
+//     "https://www.amazon.ca/*",
+//     "https://www.amazon.co.uk/*",
+//     "http://www.amazon.it/*",
+//     "https://www.amazon.it/*",
+//     "https://www.amazon.fr/*",
+//     "https://www.amazon.es/*",
+//     "https://www.amazon.in/*"
+//   ],
+//   all_frames: true
 // }
-
-export const config: PlasmoCSConfig = {
-  matches: [
-    "http://www.amazon.com/*",
-    "https://www.amazon.com/*",
-    "http://smile.amazon.com/*",
-    "https://smile.amazon.com/*",
-    "https://www.amazon.ca/*",
-    "https://www.amazon.co.uk/*",
-    "http://www.amazon.it/*",
-    "https://www.amazon.it/*",
-    "https://www.amazon.fr/*",
-    "https://www.amazon.es/*",
-    "https://www.amazon.in/*"
-    ],
-    all_frames: true,
-    world: "MAIN"
-}
-
-
-const CustomButton = () => {
-  const {getHTMLData, trigger, pageData} = useSearchContext()
-  const handleMessage = (message, sender, sendResponse) => {
-    console.log('Message received in content.jsx:', message);
-    sendResponse({message: "Response from content.jsx"})
-  }
-  React.useEffect(() => {
-    console.log('content is loaded')
-    chrome.runtime.onMessage.addListener(handleMessage)
-    return () => {
-      chrome.runtime.onMessage.removeListener(handleMessage);
-    };
-  }, [])
-    return (
-      <MySearchProvider>
-        {trigger && <div style={{backgroundColor: "white", borderRadius: "50%", padding: "0 5px 0 5px"}}>
-            <img className='!w-[35px]' style={{width: "35px"}} src={logo} alt="logo"  />
-        </div>}
-      </MySearchProvider>
-    )
-  }
-   
-  export default CustomButton
+ 
+// function DeltaTab() {
+//   const mailPort = usePort("mail")
+ 
+//   return (
+//     <div>
+//       {mailPort.data?.message}
+//       <button
+//         onClick={async () => {
+//           mailPort.send({
+//             hello: "world"
+//           })
+//         }}>
+//         Send Data
+//       </button>
+//     </div>
+//   )
+// }
+ 
+// export default DeltaTab
