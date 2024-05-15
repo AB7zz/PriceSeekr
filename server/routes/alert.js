008b413240aa9837e9deabae0394996c04885c33
AB7zz <abhinavcv007@gmail.com>
2024-05-15 22:26:50 +0530

    console new price every 30sec

--- server/routes/alert.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import Alert from '../controllers/alert/Alert.js'
-const router = express.Router()
-
-router.post('/alert', Alert)
-
-export default router
--- server/routes/index.js
+++
@@ -1,3 +0,0 @@
-import alert from './alert.js'
-
-export default [alert]
